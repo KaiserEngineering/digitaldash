54caf7ecec8f079d5f886228e4818020cdd62a4f
Craig Kaiser <craig@kaiserengineering.io>
2020-04-01 19:36:03 -0400

    Add smoothing to needle rotation

--- lib/DigitalDash/Massager.py
+++ lib/DigitalDash/Massager.py
@@ -1,4 +1,6 @@
 """Massages values being passed to gauges to smooth them."""
+from kivy.logger import Logger
+from math import *
 
 class Massager():
     """
@@ -8,19 +10,28 @@ class Massager():
     def __init__(self):
         pass
 
-    def Smooth(self, args):
+    def Smooth(self, Old, New, **args):
         """
         Smoothing transitions between data points
-            :param self: 
-            :param args: { 
-                'Current': Value,
-                 'New' : Value,
-            }
+            :param self: <DigitalDash.Massage>
+            :param 'Old': Value,
+            :param 'New' : Value,
         """
-        if not args['Current'] and not args['New']:
-            return 0
+        if not Old and not New:
+            Logger.error( "Cannot smooth without a current and new value" )
+            return New
+        delta = abs(New - Old)
+        # increment = Old + 0.75*log(-delta)
+        increment = (pow(delta,2) / pow(120, 2)) * 120
+        print(increment)
 
-        delta = abs(args['Current'] - args['New'])
-        if delta == 0:
-            return args['New']
-        return args['New'] / (delta / (0.5 * delta))
+        if ( New - Old  < 0 ):
+            return Old - increment
+        return Old + increment
+
+        # if delta <= 3:
+        #     return Old
+        # print("Delta: "+str(delta))
+        # print("New: "+str(New))
+        # print ("Calculated: "+str(New / ( delta * 0.75 )))
+        # return  ( New / ( delta *1.75 ) ) * New
