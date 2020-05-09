d885219ba33d47d378dfbde1633e95fdf175bb5f
Craig <craig@kaiserengineering.io>
2020-05-09 09:35:11 -0400

    A starting point

--- svelte/src/components/View.svelte
+++ svelte/src/components/View.svelte
@@ -1,33 +1,20 @@
 <script>
-    export let item;
-    export let returnTo;
+    import { beforeUpdate } from "svelte";
+    export let view;
 </script>
 
-<style>
-    article {
-        margin: 0 0 1em 0;
-    }
+<div class="overflow-hidden shadow-lg border-t-4 bg-white mb-4 rounded-b-lg rounded-t border-red-light w-full md:w-1/4">
+    <div class="px-6 py-4 mb-2 mt-4 mb-8">
+        <div class="uppercase tracking-wide text-c2 mb-4">{view.name}</div>
 
-    a {
-        display: block;
-        margin: 0 0 1em 0;
-    }
-
-    h1 {
-        font-size: 1.4em;
-        margin: 0;
-    }
-</style>
-
-<a href={returnTo}>&laquo; back</a>
-
-<article>
-    <a href="{url}">
-        <h1>{item.title}</h1>
-        {#if item.domain}
-            <small>{item.domain}</small>
-        {/if}
-    </a>
-
-    <p class="meta">submitted by {item.user} {item.time_ago}
-</article>
+        {#each view.gauges as gauge}
+        <div class="flex cursor-pointer border px-4 py-2 text-lg text-grey-darkest border-b-0" style="border-left: 4px solid #e2624b !important;">
+            <svg width="18" height="18" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" style="fill: #e2624b;">
+                <path d="M960 256q0-26-19-45t-45-19-45 19-19 45 19 45 45 19 45-19 19-45zm832 928v352q0 22-20 30-8 2-12 2-12 0-23-9l-93-93q-119 143-318.5 226.5t-429.5 83.5-429.5-83.5-318.5-226.5l-93 93q-9 9-23 9-4 0-12-2-20-8-20-30v-352q0-14 9-23t23-9h352q22 0 30 20 8 19-7 35l-100 100q67 91 189.5 153.5t271.5 82.5v-647h-192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h192v-163q-58-34-93-92.5t-35-128.5q0-106 75-181t181-75 181 75 75 181q0 70-35 128.5t-93 92.5v163h192q26 0 45 19t19 45v128q0 26-19 45t-45 19h-192v647q149-20 271.5-82.5t189.5-153.5l-100-100q-15-16-7-35 8-20 30-20h352q14 0 23 9t9 23z"
+                />
+            </svg>
+            <div class="pl-2">gauge</div>
+        </div>
+        {/each}
+    </div>
+</div>
