193df9286d5e9c3f5aaefde09c8244aba9ca1aee
Craig Kaiser <craig@kaiserengineering.io>
2020-05-12 15:07:32 -0400

    Remove smart quotes

--- svelte/src/views/config.svelte
+++ svelte/src/views/config.svelte
@@ -4,7 +4,7 @@
     let viewsPromise = getConfig();
 
     async function getConfig() {
-        const res = await fetch(`http://localhost:3000/api/config`);
+        const res = await fetch('http://localhost:3000/api/config');
         const config = await res.json();
 
         if (res.ok) {
