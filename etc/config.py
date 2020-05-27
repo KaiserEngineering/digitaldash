5ac27e4b06c45dd28b7a8081b74254753f549eba
craig kaiser <craig@kaiserengineering.io>
2020-05-27 13:44:38 -0400

    Add real config validator

--- etc/config.py
+++ etc/config.py
@@ -16,7 +16,7 @@ def views(file=None):
 
     Window._rotation = 0
 
-    jsonData = jsonData if validateConfig(jsonData) else json.loads('{"views": { "0": {"alerts": [],"background": "bg.jpg","dynamic": {},"gauges": [{"themeConfig": "Error","dataIndex": 0,"module": "Misc","path": "static/imgs/Alerts/"}],"name": "Error","pids": []}}}')
+    jsonData = jsonData if validateConfig(jsonData) else json.loads('{"views": { "0": {"alerts": [], "theme": "Error", "background": "bg.jpg","dynamic": {},"gauges": [{"themeConfig": "Error","dataIndex": 0,"module": "Misc","path": "static/imgs/Alerts/"}],"name": "Error", "enabled": 1, "pids": []}}}')
 
     return jsonData
 
@@ -37,11 +37,65 @@ def getThemeConfig(theme):
 def validateConfig(config):
     """Validate config."""
 
-    for view in config['views']:
-        if ( not 'name' in config['views'][view] or not config['views'][view]['name'] ):
-            Logger.error( "Name required for view" )
-            return False
-        if ( not 'theme' in config['views'][view] or not config['views'][view]['theme'] ):
-            Logger.error( "theme required for view" )
-            return False
+    required_values = {
+        "top" : {
+                "name": str,
+                "enabled" : int,
+                "theme" : str,
+                "background": str,
+                "pids": list,
+                "dynamic": dict,
+                "alerts": list,
+                "gauges": list 
+        },
+        "alerts" : {
+            "dataIndex": int,
+            "op": str,
+            "value": int,
+            "priority": int,
+            "message" : str
+        },
+        "gauges" : {
+            "themeConfig" : str,
+            "dataIndex" : int,
+            "module" : str,
+            "path" : str
+        },
+        "dynamic" : {
+            "dataIndex" : int,
+            "op" : str,
+            "priority" : int,
+            "value" : int
+        },
+    }
+
+    for id in config['views']:
+        view = config['views'][id]
+
+        # Top level keys
+        for key in required_values.keys():
+            if ( not key in view and not key == 'top' ):
+                Logger.error( key + " required for view" )
+                return False
+            if ( key == 'top' ):
+                for key in required_values['top'].keys():
+                    if ( not type( view[key] ) == required_values['top'][key] ):
+                        Logger.error( key + " must be of type " + str(required_values['top'][key]) )
+                        return False
+                    continue
+            else:
+                for item in required_values[key].keys():
+                    if ( len(view[key]) ):
+
+                        if ( type( view[key] ) == dict ):
+                            if ( not type( view[key][item] ) == required_values[key][item] ):
+                                Logger.error( item + " for " + key + "must be of type " + str(required_values[key][item]) )
+                                return False
+                            continue
+                        else:
+                            for hash in view[key]:
+                                if ( not type( hash[item] ) == required_values[key][item] ):
+                                    Logger.error( item + " for " + key + "must be of type " + str(required_values[key][item]) )
+                                    return False
+                                continue
     return True
