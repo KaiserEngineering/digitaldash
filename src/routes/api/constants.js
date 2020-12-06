ec52739aeb0b268dd436c1a2f2113f3100bd5176
craig kaiser <craig@bestpractical.com>
2020-12-06 18:57:24 -0500

    Return promise when loading constants
    Could run into the page loading too quickly before we have returned our
    constants JSON.

--- src/routes/api/constants.js
+++ src/routes/api/constants.js
@@ -4,13 +4,17 @@ const constants_path = process.env.KEGUIHome
 
 let constantsCache;
 export async function get() {
-  if ( !constantsCache ) {
-    exec( `python3 ${constants_path}/static/constants.py`, (error, stdout, stderr) => {
-      if ( error ) {
-        console.error(`exec error: ${error}`);
-      }
-      constantsCache = JSON.parse( stdout );
-    });
-  }
-  return { body: constantsCache };
+  return new Promise(function(resolve, reject){
+    if ( !constantsCache ) {
+      exec( `python3 ${constants_path}/static/constants.py`, (error, stdout, stderr) => {
+        if ( error ) {
+          reject( err );
+        }
+        else {
+          constantsCache = JSON.parse( stdout );
+          resolve({ body: constantsCache });
+        }
+      });
+    }
+  });
 }
