c3a516ee55cb00200ef015e6fb3c46990463f700
craig kaiser <craig@bestpractical.com>
2020-12-06 14:19:58 -0500

    Use exec instead of execSync

--- src/routes/api/constants.js
+++ src/routes/api/constants.js
@@ -1,11 +1,16 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 
 const constants_path = process.env.KEGUIHome
 
 let constantsCache;
 export async function get() {
   if ( !constantsCache ) {
-    constantsCache = JSON.parse( execSync( `python3 ${constants_path}/static/constants.py` ) );
+    exec( `python3 ${constants_path}/static/constants.py`, (error, stdout, stderr) => {
+      if ( error ) {
+        console.error(`exec error: ${error}`);
+      }
+      constantsCache = JSON.parse( stdout );
+    });
   }
   return { body: constantsCache };
 }
