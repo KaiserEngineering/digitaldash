0d795ce1a84fe61da02d77de08f79051e39e31c6
craig kaiser <craig@kaiserengineering.io>
2020-12-09 13:52:34 -0500

    Add simple type test for constants.py

---
+++ tests/test_ke_protocol.py
@@ -0,0 +1,7 @@
+import test
+from static.constants import KE_CP_OP_CODES
+
+def test_constants_types():
+    for value in KE_CP_OP_CODES.values():
+      assert type(value) == type(1), "Confirm OP codes are integers."
+
