d01637d4125a897443cced8a05daf21af8ffa9b5
craig kaiser <craig@bestpractical.com>
2021-01-03 16:20:26 -0500

    No need to remove id

--- src/routes/api/config.js
+++ src/routes/api/config.js
@@ -49,7 +49,6 @@ export async function put( request ) {
         break;
       }
     }
-    delete(view.id);
 
     if ( count === 0 ) {
       // Why do I need this? We obviously have some kind of object reference
