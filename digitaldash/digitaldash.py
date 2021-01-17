d424152295c618c505bb5af588e893180e23a189
Craig Kaiser <craig@bestpractical.com>
2021-01-16 22:10:46 -0500

    Move application logic out of main.py
    Main.py should have as little logic as possble related to the runtime
    of the GUI and instead just implement code to start the GUI. It makes
    more sense to move the logic for building the display into lib/.

---
+++ digitaldash/digitaldash.py
@@ -0,0 +1,184 @@
+from typing import NoReturn, List, TypeVar
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.properties import StringProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.clock import Clock
+
+from etc import config
+from digitaldash.base import Base
+from digitaldash.dynamic import Dynamic
+from digitaldash.alert import Alert
+from digitaldash.alerts import Alerts
+from ke_protocol import build_update_requirements_bytearray
+
+# Import custom gauges
+from local.gauges import *
+
+class Background(AnchorLayout):
+    """Uses Kivy language to create background."""
+
+def setup(self, Layouts):
+    """
+    Build all widgets for DigitalDash.
+
+    This method will collect config arguments for number/type and other
+    values for views. Then it will build the views and return them.
+    """
+
+    callbacks  = {}
+    views      = []
+    containers = []
+    view_count = 0
+
+    # Sort based on default value
+    for id in sorted(Layouts['views'],
+                     key=lambda id: Layouts['views'][id].get('default', 0), reverse=True):
+        view = Layouts['views'][id]
+        # Skip disabled views
+        if not view['enabled']: continue
+
+        # Get our PIDs list from the gauges, alerts and dynamic config
+        pids_dict = {}
+        for gauge in view['gauges']:
+          pids_dict[gauge['pid']] = 1
+        for alert in view['alerts']:
+          pids_dict[alert['pid']] = 1
+        if view['dynamic'] and view['dynamic']['enabled']:
+          pids_dict[view['dynamic']['pid']] = 1
+        pids = list(pids_dict.keys())
+
+        # Get our units dict from the gauges, alerts and dynamic config
+        units = {}
+        for gauge in view['gauges']:
+          units[gauge['pid']] = gauge['unit']
+        for alert in view['alerts']:
+          units[alert['pid']] = alert['unit']
+        if view['dynamic'] and view['dynamic']['enabled']:
+          units[view['dynamic']['pid']] = view['dynamic']['unit']
+
+        background = view['background']
+
+        # Create our callbacks
+        if view['dynamic'].keys():
+            dynamic = view['dynamic']
+            dynamic['index'] = view_count
+
+            dynamic_obj = Dynamic()
+            (ret, msg) = dynamic_obj.new(**dynamic)
+            if ret:
+                callbacks.setdefault('dynamic', []).append(dynamic_obj)
+            else:
+                Logger.error(msg)
+                callbacks.setdefault('dynamic', [])
+        else:
+            callbacks.setdefault('dynamic', [])
+
+        if len(view['alerts']):
+            for alert in view['alerts']:
+                alert['index'] = view_count
+
+                callbacks.setdefault(view_count, []).append(Alert(**alert))
+        else:
+            callbacks.setdefault(view_count, [])
+
+        container       = FloatLayout()
+        ObjectsToUpdate = []
+
+        num_gauges = len(view['gauges'])
+        # Get our % width that each gauge should claim
+        # The 0.05 is our squish value to move gauges inwards
+        percent_width = ( 1 / num_gauges ) - 0.05
+
+        multi_offset = 0;
+        # Only set offset if more than 1 gauge
+        if num_gauges > 1:
+            multi_offset = percent_width * ( num_gauges / 2 - 0.5 )
+
+        for gauge_count, widget in enumerate( view['gauges'] ):
+            mod = None
+
+            x_position = ( percent_width * gauge_count ) - multi_offset
+
+            # This handles our gauge positions, see the following for reference:
+            # https://kivy.org/doc/stable/api-kivy.uix.floatlayout.html#kivy.uix.floatlayout.FloatLayout
+            subcontainer = RelativeLayout(
+                pos_hint={'x': x_position, 'top': .99},
+                size_hint_max_y=200
+            )
+            container.add_widget(subcontainer)
+
+            mod = None
+            try:
+                # This loads any standalone modules
+                mod = globals()[widget['module']]()
+            except KeyError:
+                mod = Base(gauge_count=len(view['gauges']))
+            ObjectsToUpdate.append(
+                mod.build_component(
+                    working_path=self.WORKING_PATH,
+                    container=subcontainer,
+                    view_id=int(id),
+                    x_position = x_position,
+                    **widget,
+                    **view
+                )
+            )
+
+        containers.append(container)
+
+        pid_byte_code = None
+        if ( len(units) and len(pids) ):
+            if ( list(units.keys())[0] != 'n/a' and pids[0] != 'n/a' ):
+                pid_byte_code = build_update_requirements_bytearray( units, pids )
+
+        views.append({'app': Background(), 'background': background, 'alerts': FloatLayout(),
+                      'ObjectsToUpdate': ObjectsToUpdate, 'pids': pids, 'pid_byte_code': pid_byte_code})
+        view_count += 1
+    return (views, containers, callbacks)
+
+def build_from_config(self) -> NoReturn:
+    self.current = 0
+    self.first_iteration = False if hasattr(self, 'first_iteration') else True
+
+    (self.views, self.containers, self.callbacks) = setup(self, config.views(file=self.configFile))
+
+    # Sort our dynamic and alerts callbacks by priority
+    self.dynamic_callbacks = sorted(self.callbacks['dynamic'],
+                                    key=lambda x: x.priority, reverse=True)
+    # Since we are building for the first time we can default to index 0
+    self.alert_callbacks = sorted(self.callbacks[0],
+                                  key=lambda x: x.priority, reverse=True)
+
+    (self.background, self.background_source, self.alerts,
+     self.ObjectsToUpdate, self.pids, self.pid_byte_code) = self.views[0].values()
+
+    if not self.first_iteration and Data_Source and type(Data_Source) != Test:
+    #Initialize our hardware set-up and verify everything is peachy
+        (ret, msg) = Data_Source.initialize_hardware()
+
+        if not ret:
+            Logger.error("Hardware: Could not initialize hardware: " + msg)
+            count = 0
+            # Loop in the restart process until we succeed
+            while (not ret and count < 3):
+                Logger.error("Hardware: Running hardware restart, attempt :#" + str(count))
+                (ret, msg) = Data_Source.initialize_hardware()
+
+                if not ret:
+                    count = count + 1
+                    Logger.error("Hardware: Hardware restart attempt: #"+str(count)
+                                 +" failed: " + msg)
+        else:
+            Logger.info(msg)
+        self.data_source = Data_Source
+
+    self.app.add_widget(self.background)
+    self.background.add_widget(self.containers[0])
+    self.background.add_widget(self.alerts)
+
+    # Unschedule our previous clock event
+    if hasattr(self, 'clock_event'): self.clock_event.cancel()
+    if self.data_source: self.clock_event = Clock.schedule_interval(self.loop, 0)
+
+    return self.app
