0c2e8788524051a7cb779c9cfdd48643dcb75164
craig kaiser <craig@bestpractical.com>
2021-03-22 08:36:13 -0400

    Version bump, config update

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,8 @@
 const sveltePreprocess = require('svelte-preprocess');
+const node = require('@sveltejs/adapter-node');
+const pkg = require('./package.json');
+
+/** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -7,9 +11,15 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: '@sveltejs/adapter-node',
+		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: Object.keys(pkg.dependencies || {})
+			}
+		}
 	}
 };
