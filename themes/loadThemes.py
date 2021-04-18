f5e4895a858c147596b110df893cf6098590afff
Matt Kaiser <matt@kaiserengineering.io>
2021-04-17 23:31:37 -0400

    Load themes from working directory only

--- themes/loadThemes.py
+++ themes/loadThemes.py
@@ -1,15 +1,14 @@
 import os
 import json
+import pathlib
 
-with open( 'themeDirectories.txt' ) as f:
-    root = f.readlines()
+WORKING_PATH = str(pathlib.Path(__file__).parent.absolute())
 
 themes = {}
 
-for dir in root:
-    dirlist = [item for item in os.listdir(dir.rstrip()) if os.path.isdir(os.path.join(dir.rstrip(), item))]
-    for folder in dirlist:
-        themes[folder] = dir.rstrip() + '\\' + folder
+dirlist = [item for item in os.listdir(WORKING_PATH.rstrip()) if os.path.isdir(os.path.join(WORKING_PATH.rstrip(), item))]
+for folder in dirlist:
+    themes[folder] = WORKING_PATH.rstrip() + '\\' + folder
 
 with open('themes.json', 'w') as outfile:
     json.dump(themes, outfile, indent=4)
