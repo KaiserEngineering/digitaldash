9e2f7a96ae2bcbee704f1a1ec8707d98f1d99de5
craig kaiser <craig@bestpractical.com>
2021-05-25 05:59:24 -0400

    Allow for reading of multiple Kivy logs

--- src/lib/Debug.ts
+++ src/lib/Debug.ts
@@ -3,10 +3,14 @@ import fs from 'fs';
 const gui_path: String = import.meta.env.VITE_KEGUIHome;
 
 export function ReadLog() {
+  const logNames = fs.readdirSync( gui_path+'/etc/kivy/logs' );
+
+  let logHash = {};
+  logNames.forEach((log) => {
+    logHash[log] = fs.readFileSync(gui_path+'/etc/kivy/logs/'+log).toString()
+  });
+
   return {
-    headers: {
-      'Content-Type': 'text'
-    },
-    body: fs.readFileSync(gui_path+'/etc/kivy/logs/ke_log.txt').toString()
+    body: logHash
   }
 }
--- src/routes/debug/index.svelte
+++ src/routes/debug/index.svelte
@@ -3,9 +3,16 @@
     const res = await fetch( "/api/debug" );
 
     if (res.ok) {
+      let data = await res.json();
+      const fileNames = Object.keys( data ).sort();
+
       return {
         props: {
-          content: await res.text()
+          // We don't want to mutate our array, so no pop()
+          content  : data[fileNames[fileNames.length - 1]],
+          logNames : fileNames,
+          logs     : data,
+          current  : fileNames[fileNames.length - 1]
         }
       };
     }
@@ -18,23 +25,41 @@
 </script>
 
 <script>
-  export let content = "Loading...";
+  export let content  = "Loading...";
+  export let logNames = [];
+  export let logs     = {};
+  export let current  = '';
 
   function refresh() {
     fetch("/api/debug")
-      .then(d => d.text())
+      .then(d => d.json())
       .then(d => {
-        content = d;
+          content  = d[Object.keys( d ).sort().pop()],
+          logNames = Object.keys( d ),
+          logs     = d
     });
+  }
+
+  function loadContent( logFile ) {
+    content = logs[logFile];
   }
 </script>
 
-<button type="button" class="btn btn-secondary" on:click="{refresh}">Refresh</button>
+<div class="col-11">
+  <label for="logName">Log:</label>
+  <select id="logName" bind:value={current} on:change="{e => loadContent( e.target.value )}" class="custom-select form-control d-block w-100 mb-2" required>
+    {#each logNames as log}
+      <option value={log}>{log}</option>
+    {/each}
+  </select>
 
-<div class="container col-sm-12 col-md-6 border">
-  <pre>
-    <code>
-      {content}
-    </code>
-  </pre>
+  <button type="button" class="mb-2 btn btn-secondary" on:click="{refresh}">Refresh</button>
+
+  <div class="container col-sm-12 col-md-6 border">
+    <pre>
+      <code>
+        {content}
+      </code>
+    </pre>
+  </div>
 </div>
