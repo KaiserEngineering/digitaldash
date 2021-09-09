222970da064d1ac32ffb576c76b21d1b5cfd59ac
craig kaiser <craig@kaiserengineering.io>
2021-09-08 22:48:32 -0400

    Re-add offset test for radial gauges

--- tests/test_api.py
+++ tests/test_api.py
@@ -79,7 +79,7 @@ def test_needle_simple():
             needle.type + " component defaults to minimum value"
         )
         assert needle.update == smooth(
-            old=old_value, new=4000 * needle.step
+            old=old_value, new=4000 * needle.step - offset
         ), print(
             needle.type
             + " component sets the correct rotational value with smoothing (not true value)"
