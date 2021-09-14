ff4e6f898c9a079fcb8fe64f3afc733df5a46268
craig kaiser <craig@kaiserengineering.io>
2021-09-13 20:53:28 -0400

    Add initial programatically generated configs for testing

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,8 @@ from etc import config
 import os
 
 from digitaldash import digitaldash
-
+from static.constants import get_constants
+from themes.loadThemes import getThemes
 import pathlib
 
 working_path = str(pathlib.Path(__file__).parent.parent.absolute())
@@ -38,3 +39,45 @@ def test_config_file_from_cli():
             assert  ret == True, print(
                 entry.name + " passes config validation check"
             )
+
+def test_config_programatically():
+    dd = main.GUI()
+    dd.working_path = working_path
+
+    configBase = {
+        "views" : {
+            "0": {
+              "name":"base",
+              "enabled" : True,
+              "default" : 1,
+              "background":"black.png",
+              "dynamicMinMax": True,
+              "dynamic": {},
+              "alerts": [],
+              "gauges": []
+            }
+        }
+    }
+
+    themes = getThemes()
+    constants = get_constants()
+
+    for pid in constants['KE_PID']:
+      newConfig = dict(configBase)
+      newConfig['views']['0']['gauges'].append({
+        'pid' : constants['KE_PID'][pid]['name'],
+      })
+
+      for theme in themes:
+        newConfig['views']['0']['gauges'][0]['theme'] = theme
+
+        for myTuple in constants['KE_PID'][pid]['units'].items():
+            (unit, value) = (myTuple)
+            newConfig['views']['0']['gauges'][0]['unit'] = unit
+
+            (ret, msg) = config.validateConfig(newConfig)
+            assert  ret == True, print(
+                "Random config passes config validation check"
+            )
+      # Remove the last gauge we entered
+      newConfig['views']['0']['gauges'].pop()
