0fea550edb6fd4f128b57f8e7bc6e56be12fdef1
Craig Kaiser <craig@kaiserengineering.io>
2023-09-04 15:19:54 -0400

    set Access-Control-Allow-Origin header to *
    There is an issue where Android devices do not allow the hostname redirect
    for `kedigitaldash.com` which results in CORs errors when trying to access
    the web app from the PI IP address.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,21 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import type { UserConfig } from "vite";
 import { resolve } from "path";
 
+/** @type {import('vite').Plugin} */
+const viteServerConfig = {
+  name: 'add headers',
+  configureServer: (server: { middlewares: { use: (arg0: (req: any, res: any, next: any) => void) => void; }; }) => {
+    server.middlewares.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", ["GET", "POST"]);
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      next();
+    });
+  }
+};
+
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), viteServerConfig],
   css: {
     preprocessorOptions: {
       scss: {
